a6945f24e6deb6257d94dce2865756e09979284a
ottomr <husnutapan1994@gmail.com>
2019-05-16 21:02:53 +0300

    asdas

--- src/main/java/com/pipeline/demotest/DemoTestApplication.java
+++ src/main/java/com/pipeline/demotest/DemoTestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoTestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoTestApplication.class, args);
     }
-
 }
